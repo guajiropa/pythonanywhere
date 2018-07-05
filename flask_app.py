dc4da7643f9aa35d167dc4e3ba14fdd0d02b1f35
guajiropa <guajiropa@hotmail.com>
2018-07-05 04:37:35 +0000

    First commit of a simple CRUD on pythonanywhere

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
-
+#
 # A very simple Flask Hello World app for you to get started with...
+#
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
